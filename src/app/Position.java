7b5e78a02ac3e4fb462a008ffb55a9c2d4842be4
Hugo VO Moura <zoeiratheforever@gmail.com>
2023-08-21 11:58:58 -0300

    class Position criada

--- src/app/Position.java
+++ src/app/Position.java
@@ -1,5 +1,17 @@
 package app;
 
 public class Position {
+	private int row;
+	private int column;
+	
+	public void SetValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
 
+	@Override
+	public String toString() {
+		return "Position [row=" + row + ", column=" + column + "]";
+	};
+	
 }
